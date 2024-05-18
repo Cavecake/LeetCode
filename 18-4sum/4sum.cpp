9cfbf9f5abf99972c58b4f1c125710ad8b10e516
Cavecake <93793653+Cavecake@users.noreply.github.com>
2024-05-18 22:48:18 +0200

    Time: 164 ms (25.20%) | Memory: 16 MB (52.16%) - LeetSync

--- 18-4sum/4sum.cpp
+++ 18-4sum/4sum.cpp
@@ -2,23 +2,27 @@ class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
         vector<vector<int>> quadruplets;
+        sort(nums.begin(),nums.end(), less<int>());
         for(int i = 0; i < nums.size();++i){
+            if(i > 0 && nums[i] == nums[i - 1]) continue;
             for(int j = i + 1; j < nums.size(); ++j){
+                if(j > i + 1 && nums[j] == nums[j - 1]) continue;
                 for(int k = j + 1; k < nums.size(); ++k){
+                    if(k > j + 1 && nums[k] == nums[k - 1]) continue;
                     // O (n^3)
-                    long long summe = nums[i];
+                    long summe = nums[i];
                     summe += nums[j] + nums[k];
-                    if(summe > INT_MAX){
-                        continue;
-                    }
+                    //cout << nums[i] << " " << nums[j] << " " << nums[k] << " " << summe << endl;
+
                     long diff = target - summe;
+                    if(diff > INT_MAX || diff < INT_MIN) continue;
+                    //cout << diff << endl;
                     auto it = find(nums.begin() + k + 1, nums.end(), diff);
                     if(it != nums.end()){
                         int index = it - nums.begin();
-                        vector<int> quadruplet = 
-                        {nums[i], nums[j], nums[k], nums[index]};
-                        
-                        sort(quadruplet.begin(), quadruplet.end(),greater<int>());
+                        vector<int> quadruplet = {nums[i], nums[j], nums[k], nums[index]};
+
+                        //cout << nums[i] << " " << nums[j] << " " << nums[k] << " " << nums[index] << endl;
 
                         if(find(quadruplets.begin(), quadruplets.end(),quadruplet) != quadruplets.end()) continue;
                         quadruplets.push_back(quadruplet);
