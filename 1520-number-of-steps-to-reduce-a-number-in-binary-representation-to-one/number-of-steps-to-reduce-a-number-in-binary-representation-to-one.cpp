c081c125590e58e921d9ba8a807ed9316bd6a48d
Cavecake <93793653+Cavecake@users.noreply.github.com>
2024-05-29 21:30:30 +0200

    Time: 6 ms (12.79%) | Memory: 7.4 MB (93.86%) - LeetSync

---
+++ 1520-number-of-steps-to-reduce-a-number-in-binary-representation-to-one/number-of-steps-to-reduce-a-number-in-binary-representation-to-one.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int numSteps(string s) {
+        // Add to left, carry
+        // divide by two remove right;
+        int steps = 0;
+        while(s != "1"){
+            ++steps;
+            cout << s << endl;
+            if(s[s.size() - 1] == '0'){
+                s.pop_back();
+            }
+            else{
+                int index = s.size() - 1;
+                while(index >= 0){
+                    if(s[index] == '0'){
+                        s[index] = 1;
+                        break;
+                    }
+                    s[index] = '0';
+                    --index;
+                }
+                if(index == -1) s = "1" + s;
+            }
+        }
+        return steps;
+    }
+};
