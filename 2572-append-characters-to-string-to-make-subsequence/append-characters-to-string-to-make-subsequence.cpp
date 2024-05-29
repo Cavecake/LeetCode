a763cca0d0e8773d9f3da7667101d3d4ec9c5b5d
Cavecake <93793653+Cavecake@users.noreply.github.com>
2024-05-29 21:35:46 +0200

    Time: 22 ms (27.19%) | Memory: 12 MB (47.37%) - LeetSync

--- 2572-append-characters-to-string-to-make-subsequence/append-characters-to-string-to-make-subsequence.cpp
+++ 2572-append-characters-to-string-to-make-subsequence/append-characters-to-string-to-make-subsequence.cpp
@@ -3,11 +3,9 @@ public:
     int appendCharacters(string s, string t) {
         int index_t = 0;
         int size = t.size();
-        for(char character: s){
-            if(character == t[index_t]){
-                ++index_t;
-                if(index_t == size) return 0;
-            }
+        for(int i = 0; i < s.size(); ++i){
+            if(s[i] == t[index_t])++index_t;
+            if(index_t == size) return 0;
         }
         return t.size() - index_t;
     }
