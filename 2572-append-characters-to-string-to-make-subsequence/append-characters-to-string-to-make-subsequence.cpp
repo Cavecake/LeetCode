eb7fdf81c1f5106f618611c3dcf5c642d62319ba
Cavecake <93793653+Cavecake@users.noreply.github.com>
2024-05-29 21:34:41 +0200

    Time: 22 ms (27.19%) | Memory: 11.8 MB (96.84%) - LeetSync

--- 2572-append-characters-to-string-to-make-subsequence/append-characters-to-string-to-make-subsequence.cpp
+++ 2572-append-characters-to-string-to-make-subsequence/append-characters-to-string-to-make-subsequence.cpp
@@ -4,8 +4,10 @@ public:
         int index_t = 0;
         int size = t.size();
         for(char character: s){
-            if(character == t[index_t]) ++index_t;
-            if(index_t == size) return 0;
+            if(character == t[index_t]){
+                ++index_t;
+                if(index_t == size) return 0;
+            }
         }
         return t.size() - index_t;
     }
