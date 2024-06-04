e4d27df902726957dda0586c8a82e13eaa6902f8
Cavecake <93793653+Cavecake@users.noreply.github.com>
2024-06-04 18:48:09 +0200

    Time: 9 ms (16.33%) | Memory: 9.7 MB (6.93%) - LeetSync

---
+++ 409-longest-palindrome/longest-palindrome.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int longestPalindrome(string s) {
+        // pairs can be added to left and right of center, center can be one or two elements
+        unordered_set<char> letters;
+        int amount_pairs = 0;
+        
+        for(char letter:s){
+             if (letters.find(letter) == letters.end())
+             {
+                 letters.insert(letter);
+                 continue;
+             }
+            letters.erase(letter);
+            ++amount_pairs;
+        }
+        
+        if(s.size() == 2 * amount_pairs) return s.size();
+        return 2*amount_pairs + 1;
+    }
+};
